627493470cfb116e14d176d84db69b3218c41892
Andras Fulop <andrasfulop86@gmail.com>
2018-10-30 11:52:51 +0100

    added illegal constructor

--- src/SingletonPatternDemo.java
+++ src/SingletonPatternDemo.java
@@ -2,6 +2,9 @@ public class SingletonPatternDemo {
 
     public static void main(String[] args) {
 
+        //illegal construct
+        //SingleObject object = new SingleObject();
+
         //get the only available object
         SingleObject object = SingleObject.getInstance();
 
